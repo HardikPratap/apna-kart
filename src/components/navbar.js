4ab55a2f26501c2b9d128685f659f7ce78d6a047
HardikPratap <hardikpratap188@gmail.com>
2022-07-31 13:40:44 +0530

    commit

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+class Navbar extends React.Component{
+    render(){
+        return <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#" >
+          <h1>Apna-Kart</h1>
+        </a>
+      </nav>
+      
+      
+    }
+}
+export default Navbar;
