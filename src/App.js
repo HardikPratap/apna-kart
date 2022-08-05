36dc0b48391fd09f7da73fa021575444fc5f740a
HardikPratap <hardikpratap188@gmail.com>
2022-08-05 16:32:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/navbar';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList.js";
+import Footer from "./components/Footer.js";
+// import AddItem from "./components/AddItems";
 
 function App() {
+  const products = [
+    {
+      price: 99999,
+      name: "IPhone 10S Max",
+      quantity: 0,
+    },
+    {
+      price: 9999,
+      name: "Redmi Note 10S Max",
+      quantity: 0,
+    },
+  ];
   return (
     <>
-    <Navbar/>
+      <Navbar />
+      <main className="container mt-5">
+        <ProductList product={products}/>    //product is prop name 
+      </main>    
+
+      <Footer />
     </>
   );
 }
